77758565f24ca9d5f5ba243603652876c879d5cf
Denilson Carvalho <dennis.wilson3@gmail.com>
2023-05-10 15:47:03 -0300

    GET lists

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -18,14 +18,15 @@ public class GameService {
 	private GameRepository gameRepository;
 	
 	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();		
 	}
 	
-	@Transactional(readOnly = true)
-	public GameDTO findById(Long id) {
-		Game result = gameRepository.findById(id).get();
-		return new GameDTO(result);
-	}
 }
